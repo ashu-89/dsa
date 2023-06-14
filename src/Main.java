5868313b87cad0cfa07ab6b788355374c26ca3e7
Ashutosh Pathak <ashu.p89@gmail.com>
2023-06-14 21:49:51 +0530

    working

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int [] arrayToSort = {-8, 3, 5, 9, 11, 0, -5};
-        int size = 7;
+        int [] arrayToSort = {-8, 3, 5, 9, 11, 0, -5, -12};
+        int size = 8;
 
         insertionSort(arrayToSort,size);
 
@@ -18,25 +18,66 @@ public class Main {
 
     private static void insertionSort(int[] arrayToSort, int size) {
 
-        //Start with 1st element - assume it's sorted
+        //Assume we have 2 subdivisions - 1 sorted subdivision, 1 unsorted.
+        //Left most - sorted subdivision
 
+        //Start with 1st element - assume it's sorted
         //2nd till last - one by one bring in sorted, put in right place
 
         for(int i = 1; i < size; i++){
-            putElementInRightPosition(arrayToSort, i);
-        }
 
-    }
 
-    private static void putElementInRightPosition(int[] arrayToSort, int i) {
-        for(int j=i; j>0; j--){
-            if(arrayToSort[j] < arrayToSort[j-1]){
-                int temp = arrayToSort[j];
-                arrayToSort[j]=arrayToSort[j-1];
-                arrayToSort[j-1]=temp;
+            int elementToPutInPlace;
+            int j;
+
+           // putElementInRightPosition(arrayToSort, i);
+
+            elementToPutInPlace  = arrayToSort[i];
+            j = i-1;
+
+            //The order matters.
+            //Code works if we first check j>=0 and then if arrayToSort[j] > elementToPutInPlace
+            //However, if we first check whether arrayToSort[j] > elementToPutInPlace and then check for j >= 0,
+            //In this special case where last element is smaller than 1st, code breaks - array index becomes -1.
+
+            // while(arrayToSort[j] > elementToPutInPlace && j>=0) //This won't work!
+            while( j>=0 && arrayToSort[j] > elementToPutInPlace){
+
+                //only shift right - no need to swap !
+                //int temp = arrayToSort[j+1];
+                arrayToSort[j+1] = arrayToSort[j];
+                //arrayToSort[j] = temp;
+                j--;
+
             }
+
+            //put the element in it's right place
+            arrayToSort[j+1] = elementToPutInPlace;
+
         }
+
     }
+
+//    private static void putElementInRightPosition(int[] arrayToSort, int i) {
+//
+//        int elementToPutInPlace = arrayToSort[i];
+//        int j = i-1;
+//
+//        while(arrayToSort[j] > elementToPutInPlace && j >= 0){
+//
+//            //only shift right - no need to swap !
+//            //int temp = arrayToSort[j+1];
+//            arrayToSort[j+1] = arrayToSort[j];
+//            //arrayToSort[j] = temp;
+//            j--;
+//
+//        }
+//
+//        //put the element in it's right place
+//        arrayToSort[j+1] = elementToPutInPlace;
+//
+//
+//    }
 
 
 
